1b1a0af278922ff0259d6b4434f92ae6d0a4d74d
Josh Benner <jbenner@coredial.com>
2015-11-25 13:00:44 -0500

    Hide emacs toolbar my way.

--- emacs/init.el
+++ emacs/init.el
@@ -25,12 +25,9 @@
 	     :ensure t
 	     :defer t)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(tool-bar-mode nil))
+;; Hide the menu in X.
+(tool-bar-mode -1)
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
