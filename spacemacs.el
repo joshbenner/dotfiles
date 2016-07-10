20ddf69cc87e9a05fd79e0b12ef59204ade4f6ba
Josh Benner <josh@bennerweb.com>
2016-07-10 07:44:40 -0400

    Try to set cursor type in spacemacs emacs mode.

--- spacemacs.el
+++ spacemacs.el
@@ -266,7 +266,7 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
   (global-set-key (kbd "<home>") 'spacemacs/smart-move-beginning-of-line)
-  (setq-default cursor-type 'bar)
+  (setq evil-emacs-state-cursor '("chartreuse3" (bar . 2)))
   (blink-cursor-mode t)
   )
 
