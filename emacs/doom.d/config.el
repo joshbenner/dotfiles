e4c6d96d889b2eea83e6666c2634c5e310bb5088
Josh Benner <joshb@aweber.com>
2021-06-22 11:54:03 -0400

    Default to doom-one theme in doom emacs

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -27,7 +27,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-dark+)
+(setq doom-theme 'doom-one)
 
 ;; Control default size of a new window.
 (setq initial-frame-alist '((width . 150)(height . 50)))
